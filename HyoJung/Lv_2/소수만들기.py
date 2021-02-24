bc13957ef711ca924d4a614af2490aff984250b0
leemyo <leemyo_@naver.com>
2021-02-25 04:28:16 +0900

    Update 소수만들기.py

--- HyoJung/Lv_2/소수만들기.py
+++ HyoJung/Lv_2/소수만들기.py
@@ -1,22 +1,17 @@
 from itertools import combinations
 
 def isPrime(n):
-    cnt=0
     for i in range(2, n):
-        if n%i==0: cnt+=1
-    
-    if cnt==0: return True
-    return False
+        if n%i==0:
+            return False
+    return True
 
 def solution(nums):
     cnt=0
     sub_list= list(combinations(nums,3))     #Create sublist
 
     for i in sub_list:
-        sub_sum=0
-        for j in i: sub_sum+=j
-        if isPrime(sub_sum): cnt+=1
-
+        if isPrime(sum(i)): cnt+=1
     return cnt
 
 #https://blog.naver.com/leemyo_/222255489473
